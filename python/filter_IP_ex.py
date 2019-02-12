538dfd8f8927ae6fb80c2916acede623a3599189
Wickstrom <wickstrom.kristoffer@gmail.com>
2019-02-12 17:15:33 +0100

    hallo

--- python/filter_IP_ex.py
+++ python/filter_IP_ex.py
@@ -11,11 +11,11 @@ if gpu:
 else:
     path = '/home/kristoffer/data/mnist/'
 
-N = 1
-batch_size_tr = 2000
-batch_size_te = 200
+N = 10
+batch_size_tr = 32
+batch_size_te = 100
 epochs = 2
-n_n = 50
+n_n = 12
 alpha = 1.01
 number_neurons = 50
 
@@ -26,39 +26,36 @@ N_F = [3, 5, 10, 50]
 
 x_tr, y_tr, x_te, y_te = load_mnist(path, gpu)
 
-with th.autograd.profiler.profile() as pf:
-    
-    for n in range(N):
-        temp_cost, temp_score, temp_mi = [], [], []
-        for number_filters in N_F:
-    
-            cost, score, mi_sample = [], [], []
-    
-            if gpu:
-                model = CNN5L(number_filters, number_neurons, a_func).cuda()
-            else:
-                model = CNN5L(number_filters, number_neurons, a_func)
-    
-            for epoch in range(epochs):
-                cost.append(model.train_model(x_tr, y_tr, model,
-                                              batch_size_tr, gpu))
-                print('Run Number: {}'.format(n), '\n',
-                      'Number of filter is: {}'.format(number_filters), '\n',
-                      'Epoch number: {}'.format(epoch), '\n',
-                      'Cost: {}'.format(cost[-1]))
-                with th.no_grad():
-                    mi_sample.append(model.compute_mi(x_te, y_te, n_n,
-                                                      alpha, batch_size_te,
-                                                      model, gpu))
-    
-            temp_cost.append(cost)
-            temp_mi.append(mi_sample)
-    
-            temp_score.append(model.predict(x_te, y_te, model, batch_size_te, gpu))
-    
-        all_costs.append(temp_cost)
-        mi_list.append(temp_mi)
-        all_scores.append(temp_score)
-#        np.savez_compressed('/root/output/filter_IP_ex_results_50j.npz',
-#                            a=mi_list, b=all_costs, c=all_scores)
-print(pf)
+for n in range(N):
+    temp_cost, temp_score, temp_mi = [], [], []
+    for number_filters in N_F:
+
+        cost, score, mi_sample = [], [], []
+
+        if gpu:
+            model = CNN5L(number_filters, number_neurons, a_func).cuda()
+        else:
+            model = CNN5L(number_filters, number_neurons, a_func)
+
+        for epoch in range(epochs):
+            cost.append(model.train_model(x_tr, y_tr, model,
+                                          batch_size_tr, gpu))
+            print('Run Number: {}'.format(n), '\n',
+                  'Number of filter is: {}'.format(number_filters), '\n',
+                  'Epoch number: {}'.format(epoch), '\n',
+                  'Cost: {}'.format(cost[-1]))
+            with th.no_grad():
+                mi_sample.append(model.compute_mi(x_te, y_te, n_n,
+                                                  alpha, batch_size_te,
+                                                  model, gpu))
+
+        temp_cost.append(cost)
+        temp_mi.append(mi_sample)
+
+        temp_score.append(model.predict(x_te, y_te, model, batch_size_te, gpu))
+
+    all_costs.append(temp_cost)
+    mi_list.append(temp_mi)
+    all_scores.append(temp_score)
+    np.savez_compressed('/root/output/filter_IP_ex_results_10j.npz',
+                        a=mi_list, b=all_costs, c=all_scores)
--- python/load_cifar10.py
+++ python/load_cifar10.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import torch as th
 
 
 def unpickle(file):
@@ -9,25 +10,45 @@ def unpickle(file):
     return dict
 
 
-def load_cifar10(path):
+def load_cifar10(path, gpu):
 
-    data_x_tr = []
-    data_y_tr = []
+    if gpu:
+        data_x_tr = []
+        data_y_tr = []
 
-    for i in range(1, 6):
-        data_tr = unpickle(path + 'data_batch_{}'.format(i))
+        for i in range(1, 6):
+            data_tr = unpickle(path + 'data_batch_{}'.format(i))
 
-        data_x_tr.append(data_tr[b'data'].reshape(10000, 3, 32, 32))
-        data_y_tr.append(data_tr[b'labels'])
+            data_x_tr.append(data_tr[b'data'].reshape(10000, 3, 32, 32))
+            data_y_tr.append(data_tr[b'labels'])
 
-    x_tr = np.concatenate(data_x_tr)
-    y_tr = np.concatenate(data_y_tr)
+        x_tr = th.from_numpy(np.concatenate(data_x_tr)).float().cuda()
+        y_tr = th.from_numpy(np.concatenate(data_y_tr)).long().cuda()
 
-    data_te = unpickle(path + 'test_batch')
+        data_te = unpickle(path + 'test_batch')
 
-    x_te = data_te[b'data']
-    x_te = x_te.reshape(10000, 3, 32, 32)
+        x_te = data_te[b'data']
+        x_te = th.from_numpy(x_te.reshape(10000, 3, 32, 32)).float().cuda()
 
-    y_te = np.array(data_te[b'labels'])
+        y_te = th.from_numpy(np.array(data_te[b'labels'])).long().cuda()
+    else:
+        data_x_tr = []
+        data_y_tr = []
+
+        for i in range(1, 6):
+            data_tr = unpickle(path + 'data_batch_{}'.format(i))
+
+            data_x_tr.append(data_tr[b'data'].reshape(10000, 3, 32, 32))
+            data_y_tr.append(data_tr[b'labels'])
+
+        x_tr = th.from_numpy(np.concatenate(data_x_tr)).float()
+        y_tr = th.from_numpy(np.concatenate(data_y_tr)).long()
+
+        data_te = unpickle(path + 'test_batch')
+
+        x_te = data_te[b'data']
+        x_te = th.from_numpy(x_te.reshape(10000, 3, 32, 32)).float()
+
+        y_te = th.from_numpy(np.array(data_te[b'labels'])).long()       
 
     return x_tr, y_tr, x_te, y_te
