c9767f74e6f7a66ba7ffc6d10fcda3c0b8c63e6f
Wickstrom <wickstrom.kristoffer@gmail.com>
2019-02-21 13:54:23 +0100

    change nn

--- python/activation_function_ex.py
+++ python/activation_function_ex.py
@@ -15,7 +15,7 @@ N = 10
 batch_size_tr = 250
 batch_size_te = 100
 epochs = 150
-n_n = 40
+n_n = 15
 
 all_costs, all_scores, mi_list = [], [], []
 all_scores = []
@@ -40,16 +40,17 @@ for n in range(N):
         for epoch in range(epochs):
             cost.append(model.train_model(x_tr, y_tr, model,
                                           batch_size_tr, gpu))
-            print('Run Number: {}'.format(n), '\n',
-                  'Activation function is: {}'.format(a_type[a_idx]), '\n',
-                  'Epoch number: {}'.format(epoch), '\n',
-                  'Cost: {}'.format(cost[-1]))
             with th.no_grad():
                 mi_sample.append(model.compute_mi(x_te, y_te, n_n,
                                                   batch_size_te,
                                                   model, gpu))
                 score.append(model.predict(x_te, y_te, model,
                                            batch_size_te, gpu))
+            print('Run Number: {}'.format(n), '\n',
+                  'Activation function is: {}'.format(a_type[a_idx]), '\n',
+                  'Epoch number: {}'.format(epoch), '\n',
+                  'Cost: {}'.format(cost[-1]), '\n',
+                  'Acc: {}'.format(score[-1]))
 
         temp_cost.append(cost)
         temp_score.append(score)
@@ -58,5 +59,5 @@ for n in range(N):
     all_costs.append(temp_cost)
     mi_list.append(temp_mi)
     all_scores.append(temp_score)
-    np.savez_compressed('/root/output/activation_results_40.npz',
+    np.savez_compressed('/root/output/activation_results_15.npz',
                         a=mi_list, b=all_costs, c=all_scores)
