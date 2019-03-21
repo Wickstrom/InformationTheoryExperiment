a37f47bf16b27c6441698f910b3f59caa5bfe8a4
Wickstrom <wickstrom.kristoffer@gmail.com>
2019-03-21 17:17:19 +0100

    update cnn hero

--- python/cnn_hero_ex.py
+++ python/cnn_hero_ex.py
@@ -6,7 +6,7 @@ from load_mnist import load_mnist
 
 gpu = th.cuda.is_available()
 if gpu:
-    th.cuda.set_device(2)
+    th.cuda.set_device(3)
     path = '/root/data/'
 else:
     path = '/home/kristoffer/data/mnist/'
@@ -18,7 +18,7 @@ epochs = 10
 tr_size = 60000
 n_iterations = (tr_size // batch_size_tr)*epochs
 
-activation = 'relu'
+activation = 'tanh'
 
 x_tr, y_tr, x_te, y_te = load_mnist(path, gpu)
 
@@ -62,5 +62,5 @@ for n in range(N):
         mi = np.concatenate((mi, model.MI.cpu().detach().numpy().reshape(1, n_iterations, 5, 2)))
         c_out = np.concatenate((c_out, np.array(model.cost).reshape(1, -1)))
         s_out = np.concatenate((s_out, np.array(model.score).reshape(1, -1)))
-    np.savez_compressed('/root/output/cnn_test_relu.npz',
+    np.savez_compressed('/root/output/cnn_test_tanh.npz',
                         a=mi, b=c_out, c=s_out)
